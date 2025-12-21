ca388fa806b5fd36fbd9dafdb13f996da44d3cb2
Einstein <bfielefack@gmail.com>
2025-12-21 18:02:53 +0100

    hello python

---
+++ module7/lesson37/keywordList.py
@@ -0,0 +1,4 @@
+print("welcome to the world of python")
+print("key word in python are")
+import keyword
+print(keyword.kwlist)
---
+++ module7/lesson37/print.py
@@ -0,0 +1,3 @@
+print("Helo i am Einstein")
+print("the result of 3 + 5 is ",3+5)
+#  is used for commenting
