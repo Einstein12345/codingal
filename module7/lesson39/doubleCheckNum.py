36476069e9d99d6254a58f6cf5aa20c2b769798c
Einstein <bfielefack@gmail.com>
2025-12-28 17:58:06 +0100

    los 67

---
+++ module7/lesson39/doubleCheckNum.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number to check"))
+if num>50:
+    print("the number is greater than 50")
+    if num%2==0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+else:
+    print("the number is less than 50")           
