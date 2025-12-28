b239955893ee0398c8872b91b2666278fe36f6c7
Einstein <bfielefack@gmail.com>
2025-12-28 15:14:04 +0100

    swap

---
+++ module7/lesson38/acp28.py
@@ -0,0 +1,10 @@
+a=input("enter value of a")
+b=input("enter value of b")
+c=input("enter value of c")
+temp=a
+a=b
+b=temp
+b=c
+print("value of a after swaping " ,a)
+print("value of b after swaping " , b)
+print("value of c after swaping " , c)
