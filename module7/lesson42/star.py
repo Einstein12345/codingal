419bd5162f482d669024a44c7f9ed0c09f568e95
Einstein <bfielefack@gmail.com>
2026-01-10 17:45:05 +0100

    spiral

---
+++ module7/lesson42/star.py
@@ -0,0 +1,31 @@
+import turtle      #import the turtle library
+
+turtle.Screen().bgcolor("Orange")
+
+board = turtle.Turtle()
+
+# first triangle for star
+board.forward(100) # draw base.move turtle forwoard by amount =100
+
+board.left(120)    #turns turtle counter clock wise by 120 degree angel
+
+board.forward(100)
+
+board.left(120)
+
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+# second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+turtle.done()
