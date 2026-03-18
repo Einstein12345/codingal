88d77a919e6d3a8d9319f259a8b14b72042e4c8c
Einstein <bfielefack@gmail.com>
2026-03-18 20:27:42 +0100

    lcm

--- module10/lesson59/LCM.py
+++ module10/lesson59/LCM.py
@@ -1,1 +1,10 @@
 # finding lcm of 2 numbers?
+import math
+
+# Example two-digit numbers
+num1 = 12
+num2 = 18
+
+# Finding LCM
+result = math.lcm(num1, num2)
+print(f"The LCM of {num1} and {num2} is {result}")
