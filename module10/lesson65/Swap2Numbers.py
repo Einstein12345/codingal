9023f6f6094e8dfb09586d91ea87dcb39d9207a4
Einstein <bfielefack@gmail.com>
2026-04-04 18:33:40 +0200

    swapping

---
+++ module10/lesson65/Swap2Numbers.py
@@ -0,0 +1,7 @@
+# Write a Program to swap two numbers without using a third variable.
+a=33
+b=46
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=",a,"b=",b)
---
+++ module10/lesson65/swappingUsingXOR.py
@@ -0,0 +1,6 @@
+def swap (a,b):
+    a=a^b
+    b=a^b
+    a=a^b
+    print("after swapping a=",a,"b=",b)
+swap (40,55)
