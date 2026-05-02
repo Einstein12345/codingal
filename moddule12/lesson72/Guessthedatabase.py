3f22b6974330f75629a482c91da850aaed751d6a
Einstein <bfielefack@gmail.com>
2026-05-02 18:41:20 +0200

    import.pd

---
+++ moddule12/lesson72/Guessthedatabase.py
@@ -0,0 +1,11 @@
+# Look at the images given in the python notebook and answer whether it represents a relational or non-relational database.
+print("What is the type of given database:: Document-based, database that does not store data in tables with rows and columns   - \n 1)Relational Database \n 2)Non-Relational Database")
+answer = int(input("Enter your guess here..."))
+if answer ==2 :
+    print("You guessed it right!")
+else:
+    print("Unfortunately your guess was wrong.")
+
+print("\nPlease tell your mentor why you guessed this?")
+
+
